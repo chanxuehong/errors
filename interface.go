9c5a03237a45b9a981bbd42d85fe297ac05075f7
chan <chanxuehong@gmail.com>
2017-02-10 17:32:16 +0800

    func String(err error) string

--- interface.go
+++ interface.go
@@ -1,5 +1,11 @@
 package errors
 
+import (
+	"bytes"
+	"runtime"
+	"strconv"
+)
+
 type Causer interface {
 	IID_93FF6FA1EDC311E6B34F38C98633AC15()
 
@@ -27,4 +33,44 @@ type StackTracer interface {
 
 	error
 	StackTrace() []uintptr
+}
+
+func String(err error) string {
+	// TODO 优化这里
+	if err == nil {
+		return ""
+	}
+	v, ok := err.(StackTracer)
+	if !ok {
+		return err.Error()
+	}
+	stack := v.StackTrace()
+	if len(stack) == 0 {
+		return err.Error()
+	}
+	frames := runtime.CallersFrames(stack)
+
+	var buf bytes.Buffer
+	buf.WriteString(err.Error())
+
+	var (
+		frame runtime.Frame
+		more  bool
+	)
+	for {
+		frame, more = frames.Next()
+		if frame.Function != "" {
+			buf.WriteByte('\n')
+			buf.WriteString(frame.Function)
+			buf.WriteString("\n\t")
+			buf.WriteString(frame.File)
+			buf.WriteByte(':')
+			buf.WriteString(strconv.Itoa(frame.Line))
+		}
+		if !more {
+			break
+		}
+	}
+
+	return buf.String()
 }
